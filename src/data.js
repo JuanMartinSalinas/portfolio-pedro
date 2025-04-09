56727529662f4030d5ea59d30da1046cdb2a9629
JuanMartinSalinas <juanmartinsalinas1@gmail.com>
2025-04-09 19:48:18 -0300

    data changes

--- src/data.js
+++ src/data.js
@@ -22,70 +22,22 @@ const data = [
         vid:"/Video_SteampunkSpider.mp4",
         software:[["3D Max ","Modelado 3D del robot.","/technologies/3DsMax.svg"],["After Effects","Composición final, integración 3D, efectos especiales y matte painting.","/technologies/AfterEffects.svg"],["Cinema 4D","Animación y render del robot.","/technologies/cinema4d.svg"],["PF Track","Tracking de cámara del video.","/technologies/PFTrack.svg"],["Substance","Texturizado detallado.","/technologies/Substance.svg"]],
     },
-    {
-        id:3,
-        titulo:"Lorem ipsum",
-        techList:["develop","motion"],
-        techs:["logos/Develop_Color.svg","logos/Motion_Color.svg"],
-        descripcion:"Suspendisse lacus purus, porttitor in pretium at, laoreet sit amet nunc. Integer eget vestibulum metus. Pellentesque rutrum quam et eros placerat iaculis. Proin a dolor nunc",
-        picture:"/image.jpg",
-        vid:"",
-        software:["software 1","software 2","software 3"],
-        collabs:[["john","first"],["pedro","second"]],
-        boss:{
-            bossName:"bossname",
-            bossDesc:"lorem ipsum",
-            bossImg:""
-        }
-    },
-    {
-        id:4,
-        titulo:"Lorem ipsum",
-        techList:["graphic","develop","photoshop"],
-        techs:["logos/Graph_Color.svg","logos/Develop_Color.svg","logos/Photoshop_Color.svg"],
-        descripcion:"Suspendisse lacus purus, porttitor in pretium at, laoreet sit amet nunc. Integer eget vestibulum metus. Pellentesque rutrum quam et eros placerat iaculis. Proin a dolor nunc",
-        picture:"/image.jpg",
-        vid:"",
-        software:["software 1","software 2","software 3"],
-        collabs:[["john","first"],["pedro","second"]],
-        boss:{
-            bossName:"bossname",
-            bossDesc:"lorem ipsum",
-            bossImg:""
-        }
-    },
-    {
-        id:5,
-        titulo:"Lorem ipsum",
-        techList:["three","photoshop"],
-        techs:["logos/3D_Color.svg","logos/Photoshop_Color.svg"],
-        descripcion:"Suspendisse lacus purus, porttitor in pretium at, laoreet sit amet nunc. Integer eget vestibulum metus. Pellentesque rutrum quam et eros placerat iaculis. Proin a dolor nunc",
-        picture:"/image.jpg",
-        vid:"",
-        software:["software 1","software 2","software 3"],
-        collabs:[["john","first"],["pedro","second"]],
-        boss:{
-            bossName:"bossname",
-            bossDesc:"lorem ipsum",
-            bossImg:""
-        }
-    },
-    {
-        id:6,
-        titulo:"Lorem ipsum",
-        techList:["three","photoshop"],
-        techs:["logos/3D_Color.svg","logos/Photoshop_Color.svg"],
-        descripcion:"Suspendisse lacus purus, porttitor in pretium at, laoreet sit amet nunc. Integer eget vestibulum metus. Pellentesque rutrum quam et eros placerat iaculis. Proin a dolor nunc",
-        picture:"/image.jpg",
-        vid:"",
-        software:["software 1","software 2","software 3"],
-        collabs:[["john","first"],["pedro","second"]],
-        boss:{
-            bossName:"bossname",
-            bossDesc:"lorem ipsum",
-            bossImg:""
-        }
-    },
+    // {
+    //     id:3,
+    //     titulo:"Lorem ipsum",
+    //     techList:["develop","motion"],
+    //     techs:["logos/Develop_Color.svg","logos/Motion_Color.svg"],
+    //     descripcion:"Suspendisse lacus purus, porttitor in pretium at, laoreet sit amet nunc. Integer eget vestibulum metus. Pellentesque rutrum quam et eros placerat iaculis. Proin a dolor nunc",
+    //     picture:"/image.jpg",
+    //     vid:"",
+    //     software:["software 1","software 2","software 3"],
+    //     collabs:[["john","first"],["pedro","second"]],
+    //     boss:{
+    //         bossName:"bossname",
+    //         bossDesc:"lorem ipsum",
+    //         bossImg:""
+    //     }
+    // },
 ]
 
 
